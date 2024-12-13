ac654ace6924ceb3de5f982f4fb1ccacb9cd1fda
gspider8 <83229711+gspider8@users.noreply.github.com>
2024-12-12 19:00:49 -0500

    more iam fixes

--- examples/main/main.tf
+++ examples/main/main.tf
@@ -49,6 +49,6 @@ output "django-cdn-cdn_domain" {
   value = module.django-cdn.cdn.domain_name
 }
 
-output "django-cdn-iam_credentials" {
-  value = "${module.django-cdn.iam_user_access.id}, (secret)"
-}
+# output "django-cdn-iam_credentials" {
+#   value = "${module.django-cdn.iam_user_access.id}, (secret)"
+# }
